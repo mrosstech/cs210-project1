62e3fb1cbf1da76d7f8181c06247b22705f9b416
Michael Ross <mrosstech@gmail.com>
2021-05-18 17:12:35 -0400

    working on it

--- time.cpp
+++ time.cpp
@@ -1,5 +1,43 @@
+// Time display application
+// 5/18/21
+// Author: Michael Ross
+
 #include <stdio.h>
+#include <iostream>
+#include <string.h>
+
+void DrawMenu();
+void DrawTime(bool twelve_hour, int hours, int minutes, int seconds);
+using namespace std;
+
+string GetInput();
 
 int main() {
+    int seconds = 0;
+    int minutes = 0;
+    int hours = 0;
+    char userInput;
 
+    // Main code loop
+    do {
+        // Display clocks
+        DrawMenu();
+        DrawTime(true, hours, minutes, seconds);
+        DrawTime(false, hours, minutes, seconds);
+
+
+        // Get user input
+
+
+    } while (userInput != '4');
+
+    return 0;
+
+}
+
+void DrawMenu() {
+    cout << "**************************";
+    cout << "*                        *";
+    cout << "*                        *";
+    cout << "**************************";
 }
