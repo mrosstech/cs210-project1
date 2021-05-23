8444deadb618884700bc09ea198f953242033f57
Michael Ross <mrosstech@gmail.com>
2021-05-23 17:50:43 -0400

    updated with functions

--- time.cpp
+++ time.cpp
@@ -5,31 +5,51 @@
 #include <stdio.h>
 #include <iostream>
 #include <string.h>
+#include <chrono>
+#include <curses.h>
+#include <unistd.h>
 
 void DrawMenu();
 void DrawTime(bool twelve_hour, int hours, int minutes, int seconds);
+char GetInput();
+void UpdateSeconds();
+int GetCurrentTime(int &hours, int &minutes, int &seconds);
+
 using namespace std;
 
-string GetInput();
-
 int main() {
+    // Hours / minutes and seconds will be tracked on a 24 hour basis
+    // and will be converted to show in 12 hour.
     int seconds = 0;
     int minutes = 0;
     int hours = 0;
     char userInput;
+    unsigned int delay = 1000;
+    // Initialize time to current time
+    GetCurrentTime(hours, minutes, seconds);
 
     // Main code loop
     do {
+        // Clear Screen
+        system("clear");
+
+        // Wait 1 second
+        usleep(1000000);
+
+        // Get input
         // Display clocks
-        DrawMenu();
         DrawTime(true, hours, minutes, seconds);
         DrawTime(false, hours, minutes, seconds);
 
 
         // Get user input
+        if (GetInput()) {
+            DrawMenu();
+            userInput = GetInput();
+        }
 
 
-    } while (userInput != '4');
+    } while (!getch());
 
     return 0;
 
