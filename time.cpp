6f51d5d4e7a29bd274c9ebd2f143402e16d080ef
Michael Ross <mrosstech@gmail.com>
2021-05-23 21:45:40 -0400

    need to debug

--- time.cpp
+++ time.cpp
@@ -8,12 +8,20 @@
 #include <chrono>
 #include <curses.h>
 #include <unistd.h>
+#include <iomanip>
+#include <cctype>
+#include <cstring>
 
-void DrawMenu();
-void DrawTime(bool twelve_hour, int hours, int minutes, int seconds);
+int DoMenu();
 char GetInput();
-void UpdateSeconds();
+
+void DrawTime(int hours, int minutes, int seconds);
+void UpdateSeconds(int &hours, int &minutes, int &seconds);
+void UpdateMinutes(int &hours, int &minutes, int &seconds);
+void UpdateHours(int &hours, int &minutes, int &seconds);
+
 int GetCurrentTime(int &hours, int &minutes, int &seconds);
+
 
 using namespace std;
 
@@ -24,6 +32,7 @@ int main() {
     int minutes = 0;
     int hours = 0;
     char userInput;
+    char menuInput;
     unsigned int delay = 1000;
     // Initialize time to current time
     GetCurrentTime(hours, minutes, seconds);
@@ -32,32 +41,108 @@ int main() {
     do {
         // Clear Screen
         system("clear");
+        
+        // Draw the time on the screen
+        DrawTime(hours, minutes, seconds);
+
+        // Get user keystrokes
+        userInput = getch();
+
+        // If user entered a keystroke then do the menu
+        if (!isblank(userInput)) {
+            menuInput = DoMenu();
+
+            switch (menuInput) {
+                case 1:
+                    UpdateHours(hours, minutes, seconds);
+                    break;
+                case 2:
+                    UpdateMinutes(hours, minutes, seconds);
+                    break;
+                case 3:
+                    UpdateSeconds(hours, minutes, seconds);
+            }
+        }
 
         // Wait 1 second
         usleep(1000000);
 
-        // Get input
-        // Display clocks
-        DrawTime(true, hours, minutes, seconds);
-        DrawTime(false, hours, minutes, seconds);
+        // Add a second
+        UpdateSeconds(hours, minutes, seconds);
 
-
-        // Get user input
-        if (GetInput()) {
-            DrawMenu();
-            userInput = GetInput();
-        }
-
-
-    } while (!getch());
+    } while (menuInput != 4);
 
     return 0;
 
 }
 
-void DrawMenu() {
-    cout << "**************************";
-    cout << "*                        *";
-    cout << "*                        *";
-    cout << "**************************";
+void DrawTime(int hours, int minutes, int seconds) {
+    int th_hours = hours - 12;
+    int th_minutes = minutes;
+    int th_seconds = seconds;
+
+    if (th_hours = 0) {
+        th_hours = 12;
+    }
+
+    cout << "**************************    **************************\n";
+    cout << "*      12-Hour Clock     *    *      24-Hour Clock     *\n";
+    cout << setfill('0') << setw(2) << "*      " << th_hours << ":" << th_minutes << ":" << th_seconds << "       *    ";
+    cout << setfill('0') << setw(2) << "*      " << hours << ":" << minutes << ":" << seconds << "       *\n";
+    cout << "**************************" << endl;
+}
+
+int GetCurrentTime(int &hours, int &minutes, int &seconds) {
+    // Get the current time in seconds since 1970
+    const time_t now = time(0);
+    
+    // Use the initial 'now' time to create a local time object
+    const tm localTime = *localtime(addressof(now));
+    
+    // Set the hours, minutes, and seconds references to update the variables in the main loop.
+    hours = localTime.tm_hour;
+    minutes = localTime.tm_min;
+    seconds = localTime.tm_sec;
+}
+
+void UpdateSeconds(int &hours, int &minutes, int &seconds) {
+    if (seconds == 59) {
+        seconds = 0;
+        UpdateMinutes(hours, minutes, seconds);
+    } else {
+        seconds += 1;
+    }
+}
+void UpdateMinutes(int &hours, int &minutes, int &seconds) {
+    if (minutes == 59) {
+        minutes = 0;
+        UpdateHours(hours, minutes, seconds);
+    } else {
+        minutes += 1;
+    }
+}
+void UpdateHours(int &hours, int &minutes, int &seconds) {
+    if (hours == 24) {
+        hours = 0;
+    } else {
+        hours += 1;
+    }
+}
+
+int DoMenu() {
+    int userInput = 0;
+
+    cout << "**************************" << endl;
+    cout << "* 1 - Add One Hour       *" << endl;
+    cout << "* 2 - Add One Minute     *" << endl;
+    cout << "* 3 - Add One Second     *" << endl;
+    cout << "* 4 - Exit Program       *" << endl;
+    cout << "**************************" << endl;
+
+    while (userInput < 1 && userInput > 4) {
+        cin >> userInput;
+        if (userInput < 1 || userInput > 4) {
+            cout << "Please enter valid input." << endl;
+        }
+    }
 }
